2c90392ab78384bc18b1be63c09707f5ddfa483e
Hideki Natsumi <96385473+HidekiNatsumi@users.noreply.github.com>
2024-03-03 02:43:11 +0100

    feat(gin): completion of gin project generation and also an example of a gin project which is dockerized

--- examples/ecommerce-application-server/db/migrations/ecommerce-application-server_up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE users (
-                       id bigserial PRIMARY KEY,
-                       username varchar,
-                       password varchar,
-                       email varchar
-);
